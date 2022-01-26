31873fa23bfbf23b27af618bf1538f560550624d
mahdavy <mahdavy1379@gmail.com>
2022-01-26 18:13:01 +0330

    grid

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('container');
+
+for (let i = 0; i < 256; i++) {
+    const gridElement = document.createElement('div');
+    container.appendChild(gridElement);
+  }
